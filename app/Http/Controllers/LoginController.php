d7c8baa82f9be3be521bbfb5c25076315be3722e
Teyow <61595136+Teyow@users.noreply.github.com>
2023-05-12 20:36:13 +0800

    LOGIN FUNCTION

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('username', 'password');
+        if (Auth::attempt($credentials)) {
+
+            return redirect('homepage');
+        } else {
+            return redirect('/');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+
 Route::get('/login', function () {
     return view('auth/login');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/logout', [App\Http\Controllers\LoginController::class, 'logout'])->name('logout');
